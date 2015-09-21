b879aa97001152f54736ef98a87276ffd1bc5263
Vitorio S <vitorio.s@bocistudio.com>
2015-09-21 13:50:43 +0700

    EDIT ImageView activity_edit and activity_add -> RoundImageView ADD AddActivity and EditActivity -> Chooser camera or galery and cropper

--- app/src/main/java/com/example/android/phonebook/AddActivity.java
+++ app/src/main/java/com/example/android/phonebook/AddActivity.java
@@ -1,24 +1,40 @@
 package com.example.android.phonebook;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddActivity extends Activity {
     public static int REQUEST_IMAGE_CAPTURE;
+    private Uri mImageCaptureUri;
+    private static final int PICK_FROM_CAMERA = 1;
+    private static final int CROP_FROM_CAMERA = 2;
+    private static final int PICK_FROM_FILE = 3;
+    private AlertDialog dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +51,155 @@ public class AddActivity extends Activity {
     public void cancel(View view){
         finish();
 
+    }
+
+    private void doCrop() {
+        final ArrayList<CropOption> cropOptions = new ArrayList<CropOption>();
+        /**
+         * Open image crop app by starting an intent
+         * ‘com.android.camera.action.CROP‘.
+         */
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setType("image/*");
+
+        /**
+         * Check if there is image cropper app installed.
+         */
+        List<ResolveInfo> list = getPackageManager().queryIntentActivities(
+                intent, 0);
+
+        int size = list.size();
+
+        /**
+         * If there is no image cropper app, display warning message
+         */
+        if (size == 0) {
+
+            Toast.makeText(this, "Can not find image crop app",
+                    Toast.LENGTH_SHORT).show();
+
+            return;
+        } else {
+            /**
+             * Specify the image path, crop dimension and scale
+             */
+            intent.setData(mImageCaptureUri);
+
+            intent.putExtra("outputX", 200);
+            intent.putExtra("outputY", 200);
+            intent.putExtra("aspectX", 1);
+            intent.putExtra("aspectY", 1);
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+            /**
+             * There is posibility when more than one image cropper app exist,
+             * so we have to check for it first. If there is only one app, open
+             * then app.
+             */
+
+            if (size == 1) {
+                Intent i = new Intent(intent);
+                ResolveInfo res = list.get(0);
+
+                i.setComponent(new ComponentName(res.activityInfo.packageName,
+                        res.activityInfo.name));
+
+                startActivityForResult(i, CROP_FROM_CAMERA);
+            } else {
+                /**
+                 * If there are several app exist, create a custom chooser to
+                 * let user selects the app.
+                 */
+                for (ResolveInfo res : list) {
+                    final CropOption co = new CropOption();
+
+                    co.title = getPackageManager().getApplicationLabel(
+                            res.activityInfo.applicationInfo);
+                    co.icon = getPackageManager().getApplicationIcon(
+                            res.activityInfo.applicationInfo);
+                    co.appIntent = new Intent(intent);
+
+                    co.appIntent
+                            .setComponent(new ComponentName(
+                                    res.activityInfo.packageName,
+                                    res.activityInfo.name));
+
+                    cropOptions.add(co);
+                }
+
+                CropOptionAdapter adapter = new CropOptionAdapter(
+                        getApplicationContext(), cropOptions);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Choose Crop App");
+                builder.setAdapter(adapter,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int item) {
+                                startActivityForResult(
+                                        cropOptions.get(item).appIntent,
+                                        CROP_FROM_CAMERA);
+                            }
+                        });
+
+                builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+
+                        if (mImageCaptureUri != null) {
+                            getContentResolver().delete(mImageCaptureUri, null,
+                                    null);
+                            mImageCaptureUri = null;
+                        }
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+
+                alert.show();
+            }
+        }
+    }
+
+    public void captureImageInitialization(View view){
+        final String[] items = new String[]{"Take from camera","Select from gallery"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.select_dialog_item,items);
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Select Image");
+        builder.setAdapter(adapter, new DialogInterface.OnClickListener(){
+            public void onClick(DialogInterface dialog, int item){
+                if(item==0){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    mImageCaptureUri = Uri.fromFile(new File(Environment
+                            .getExternalStorageDirectory(), "tmp_avatar_"
+                            + String.valueOf(System.currentTimeMillis())
+                            + ".jpg"));
+
+                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
+                            mImageCaptureUri);
+
+                    try{
+                        intent.putExtra("return-data",true);
+                        startActivityForResult(intent,PICK_FROM_CAMERA);
+                    }catch (ActivityNotFoundException e){
+                        e.printStackTrace();
+                    }
+
+                }else{
+                    Intent intent = new Intent();
+
+                    intent.setType("image/*");
+                    intent.setAction(Intent.ACTION_GET_CONTENT);
+
+                    startActivityForResult(Intent.createChooser(intent,
+                            "Complete action using"), PICK_FROM_FILE);
+                }
+            }
+        });
+
+        dialog = builder.create();
+        dialog.show();
     }
 
     public void submitAdd(View view){
@@ -58,36 +223,85 @@ public class AddActivity extends Activity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
-
     }
 
-    protected void onActivityResult(int requestCode,int resultCode, Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView iv = (ImageView)findViewById(R.id.add_image_view);
+        if (resultCode != RESULT_OK)
+            return;
 
-        if(requestCode==REQUEST_IMAGE_CAPTURE){
-            try{
-                photo = (Bitmap) data.getExtras().get("data");
+        switch (requestCode) {
+            case PICK_FROM_CAMERA:
+                /**
+                 * After taking a picture, do the crop
+                 */
+                doCrop();
 
-            }catch(NullPointerException ex){
-                photo = BitmapFactory.decodeFile(photofile.getAbsolutePath());
-            }
-            Save s = new Save();
-            if(photo!=null){
+                break;
 
-                ImageView iv = (ImageView)findViewById(R.id.add_image_view);
+            case PICK_FROM_FILE:
+                /**
+                 * After selecting image from files, save the selected path
+                 */
+                mImageCaptureUri = data.getData();
 
-                iv.setImageBitmap(photo);
+                doCrop();
 
-                s.SaveImage(this, photo);
-            }else{
+                break;
 
-                Toast.makeText(this, "oops cant save photo", Toast.LENGTH_LONG).show();
+            case CROP_FROM_CAMERA:
+                Bundle extras = data.getExtras();
+                /**
+                 * After cropping the image, get the bitmap of the cropped image and
+                 * display it on imageview.
+                 */
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
 
-            }
+                    iv.setImageBitmap(photo);
+                }
+
+                File f = new File(mImageCaptureUri.getPath());
+                /**
+                 * Delete the temporary image
+                 */
+                if (f.exists())
+                    f.delete();
+
+                break;
+
         }
-
-
     }
+
+//    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+//        super.onActivityResult(requestCode,resultCode,data);
+//
+//        if(requestCode==REQUEST_IMAGE_CAPTURE){
+//            try{
+//                photo = (Bitmap) data.getExtras().get("data");
+//
+//            }catch(NullPointerException ex){
+//                photo = BitmapFactory.decodeFile(photofile.getAbsolutePath());
+//            }
+//            Save s = new Save();
+//            if(photo!=null){
+//
+//                ImageView iv = (ImageView)findViewById(R.id.add_image_view);
+//
+//                iv.setImageBitmap(photo);
+//
+//
+//                s.SaveImage(this, photo);
+//            }else{
+//
+//                Toast.makeText(this, "oops cant save photo", Toast.LENGTH_LONG).show();
+//
+//            }
+//        }
+//
+//
+//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/com/example/android/phonebook/CropOption.java
@@ -0,0 +1,13 @@
+package com.example.android.phonebook;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by bocist-8 on 21/09/15.
+ */
+public class CropOption {
+    public CharSequence title;
+    public Drawable icon;
+    public Intent appIntent;
+}
